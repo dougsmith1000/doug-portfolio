30289b5bdb126f0a8ef49ffd95fb9d540091a914
Doug Smith <dougsmith1@gmail.com>
2025-02-13 21:58:33 -0500

    try different config

--- app.config.ts
+++ app.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "@solidjs/start/config";
 export default defineConfig({
   server: {
     preset: "cloudflare_module",
-    rollupConfig: {
-      external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
-    },
+    // rollupConfig: {
+    //   external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
+    // },
   },
 });
