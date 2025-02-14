f0522f52a0cedb3fb2701704c658acf633a0183d
Doug Smith <dougsmith1@gmail.com>
2025-02-13 22:12:20 -0500

    update config for cf base

--- app.config.ts
+++ app.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
   server: {
-    preset: "cloudflare_module",
-    // rollupConfig: {
-    //   external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
-    // },
+    preset: "cloudflare-pages",
+
+    rollupConfig: {
+      external: ["node:async_hooks"],
+    },
   },
 });
